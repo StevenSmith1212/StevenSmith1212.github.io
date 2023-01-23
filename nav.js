0a9a0f506eb3c72c9d0e4d0c6868b97ce73a5a6e
Steven Smith <smit3841@pnw.edu>
2023-01-23 15:04:25 -0600

    initial commit

---
+++ nav.js
@@ -0,0 +1,12 @@
+document.getElementById("nav").innerHTML = 
+    "<link rel = stylesheet href = 'style.css'> \
+    <div id = 'nav'> \
+    <a href = './home.html'> Home </a> \
+    <a href = './kudo.html'> Kudo </a> \
+    <a href = './hashirr.html'> Hashirr </a> \
+    <a href = './about.html'> About </a>  \
+    </div>"
+
+
+
+document.createElement
