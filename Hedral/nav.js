8ca107b907116653e2de75fef48d15c321777eb3
Steven <smit3841@pnw.edu>
2023-02-10 11:55:29 -0600

    combine Port and Hedral

--- Hedral/nav.js
+++ Hedral/nav.js
@@ -1,7 +1,7 @@
 document.getElementById("nav").innerHTML = 
-    "<link rel = stylesheet href = 'style.css'> \
+    "<link rel = stylesheet href = './style.css'> \
     <div id = 'nav'> \
-    <a href = './index.html'> Home </a> \
+    <a href = './TTHome.html'> Home </a> \
     <a href = './kudo.html'> Kudo </a> \
     <a href = './hashirr.html'> Hashirr </a> \
     <a href = './about.html'> About </a>  \
---
+++ Portfolio/nav.js
@@ -0,0 +1,11 @@
+document.getElementById("nav").innerHTML = 
+    "<link rel = stylesheet href = './style.css'> \
+    <div id = 'nav'> \
+        <div class = 'triangle' ></div>  \
+        <a href = './PortHome.html'> Home </a> \
+        <a href = './ed.html'> Education </a> \
+        <a href = './proj.html'> Projects </a> \
+        <a href = './ab.html'> About </a>  \
+    </div>"
+
+
