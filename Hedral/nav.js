771019389ae1ca33c73096974773c35bac1461bb
Steven Smith <smit3841@pnw.edu>
2023-03-29 10:37:56 -0500

    added Hedral page and style

--- Hedral/nav.js
+++ Hedral/nav.js
@@ -3,6 +3,7 @@ document.getElementById("nav").innerHTML =
     <div id = 'nav'> \
     <a href = './TTHome.html'> Home </a> \
     <a href = './hashirr.html'> Hashirr </a> \
+    <a href = './urbanfantasy.html'> The Promethean </a> \
     <a href = './about.html'> About </a>  \
     </div>"
 
